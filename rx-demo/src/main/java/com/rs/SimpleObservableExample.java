16fbd04dd57400909f907b2aec0222d436fce81e
ritesh <ritesh.sonawala@gmail.com>
2019-04-20 07:09:46 +0100

    Simple Observer

--- rx-demo/src/main/java/com/rs/SimpleObservableExample.java
+++ rx-demo/src/main/java/com/rs/SimpleObservableExample.java
@@ -7,6 +7,7 @@ public class SimpleObservableExample {
     public static void main(String[] args) {
         Observable<Integer> observable = Observable.just(Integer.valueOf(42));
         observable.subscribe(System.out::println);
+
     }
 
 }
